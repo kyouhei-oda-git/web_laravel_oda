baa7c4d2188221b0006f2753b0eadc4647986ab0
kyouhei-oda-git <kyouhei.oda.git@gmail.com>
2022-08-23 17:03:48 +0900

    クライアントサイドの入力チェックの作成：ラジオボタンでの入力チェック実装

--- public/js/input.js
+++ public/js/input.js
@@ -2,20 +2,178 @@
 var form = document.forms[0],
     input = document.getElementById('employee_id'),
     first_name = document.getElementById("first_name"),
+    last_name = document.getElementById("last_name"),
     submit = document.getElementById('submit');
+department_list = document.getElementById('department_list');
+mail = document.getElementById('mail');
+//gender = document.getElementsByName('gender');
+submit = document.getElementById('submit');
+form = document.forms.employee_text;
+r_female = document.getElementById('r_female');
+r_male = document.getElementById('r_male');
+radio = document.getElementById("radio");
+genders = document.querySelectorAll(".genders");
 
-console.log(input);
-console.log(first_name);
-console.log(submit);
-
+console.log(radio);
+console.log(genders);
+//console.log(r_female);
+//console.log(r_male);
+//社員IDの入力チェック
 input.addEventListener('invalid', function (e) {
     if (input.validity.valueMissing) {
-        e.target.setCustomValidity("社員IDを入力してください。");
-    } else if (!input.validity.valid) {
-        e.target.setCustomValidity("This is not a valid username");
+        e.target.setCustomValidity("社員IDを入力してください");
+    } else if (input.validity.tooShort) {
+        e.target.setCustomValidity("社員IDは10文字で入力してください");
+    } else if (input.validity.tooLong) {
+        e.target.setCustomValidity("社員IDは10文字で入力してください");
+    } else if (input.validity.patternMismatch) {
+        e.target.setCustomValidity("社員IDを正しく入力してください");
     }
-    // to avoid the 'sticky' invlaid problem when resuming typing after getting a custom invalid message
+
+    // 入力時のチェック
     input.addEventListener('input', function (e) {
+        e.target.setCustomValidity('');
+        if (input.validity.tooShort) {
+            e.target.setCustomValidity("社員IDは10文字で入力してください");
+        } else if (input.validity.tooLong) {
+            e.target.setCustomValidity("社員IDは10文字で入力してください");
+        } else if (input.validity.patternMismatch) {
+            e.target.setCustomValidity("社員IDを正しく入力してください");
+        }
+
+    });
+}, false);
+
+//社員名の入力チェック
+first_name.addEventListener('invalid', function (e) {
+    if (first_name.validity.valueMissing) {
+        e.target.setCustomValidity("社員名を入力してください");
+    } else if (first_name.validity.tooLong) {
+        e.target.setCustomValidity("社員名は25文字以内で入力してください");
+    }
+    // 入力時のチェック
+    first_name.addEventListener('input', function (e) {
+        e.target.setCustomValidity('');
+        if (first_name.validity.tooLong) {
+            e.target.setCustomValidity("社員名は25文字以内で入力してください");
+        }
+    });
+}, false);
+
+last_name.addEventListener('invalid', function (e) {
+    if (last_name.validity.valueMissing) {
+        e.target.setCustomValidity("社員名を入力してください");
+    } else if (last_name.validity.tooLong) {
+        e.target.setCustomValidity("社員名は25文字以内で入力してください");
+    }
+    // 入力時のチェック
+    last_name.addEventListener('input', function (e) {
+        e.target.setCustomValidity('');
+        if (last_name.validity.tooLong) {
+            e.target.setCustomValidity("社員名は25文字以内で入力してください");
+        }
+    });
+}, false);
+
+//所属セクションの入力チェック
+department_list.addEventListener('invalid', function (e) {
+    if (department_list.validity.valueMissing) {
+        e.target.setCustomValidity("所属セクションを入力してください");
+    }
+    department_list.addEventListener('input', function (e) {
         e.target.setCustomValidity('');
     });
 }, false);
+
+//メールアドレスのチェック
+mail.addEventListener('invalid', function (e) {
+    if (mail.validity.valueMissing) {
+        e.target.setCustomValidity("メールアドレスを入力してください");
+    } else if (mail.validity.tooLong) {
+        e.target.setCustomValidity("メールアドレスは256文字以内で入力してください");
+    } else if (mail.validity.patternMismatch) {
+        e.target.setCustomValidity("メールアドレスを正しく入力してください");
+    }
+    //メールアドレスの入力時チェック
+    mail.addEventListener('input', function (e) {
+        e.target.setCustomValidity('');
+        if (mail.validity.patternMismatch) {
+            e.target.setCustomValidity("メールアドレスを正しく入力してください");
+        }
+    });
+}, false);
+
+/*for (var i = 0; i < gender.length; i++) {
+    gender[i].addEventListener('invalid', function (e) {
+        console.log(gender);
+        if (!gender[i].cheaked) {
+            console.log('hoge');
+            e.target.setCustomValidity("性別を入力してください");
+        }
+    }, false);
+}*/
+genders.forEach(function (gender) {
+    gender.addEventListener('invalid', function (e) {
+        e.target.setCustomValidity('');
+        console.log(genders[0]);
+        if (r_female.validity.valueMissing && r_male.validity.valueMissing) {
+            console.log('hoge');
+            e.target.setCustomValidity("性別を入力してください");
+        }
+    });
+}, false);
+/*r_female.addEventListener('invalid', function (e) {
+    console.log('hoge')
+    if (r_female.validity.valueMissing) {
+        console.log('hoge')
+        e.target.setCustomValidity("性別を入力してください");
+    }
+    r_male.ddEventListener('invalid', function (e) {
+        console.log('hoge')
+        if (r_male.validity.valueMissing) {
+            console.log('hoge')
+            e.target.setCustomValidity("性別を入力してください");
+        }
+    })
+
+}, false);
+/*for (var i = 0; i < gender.lenngth; i++) {
+    console.log('hoge')
+    gender[i].addEventListener('invalid', function (e) {
+        if (gender[i].validity.valueMissing) {
+            console.log('hoge');
+            e.target.setCustomValidity("性別を入力してください");
+        }
+    }, false);
+}*/
+
+
+
+/*radio.addEventListener('invalid', function (e) {
+    if (gender.validity.valueMissing) {
+        console.log('hoge');
+        e.target.setCustomValidity("性別を入力してください");
+    }
+
+}, false);
+
+
+
+/*-gender.forEach(e =>
+    e.addEventListener('invalid', () => {
+        if (gender.validity.valueMissing) {
+            console.log('hoge');
+            e.target.setCustomValidity("性別を入力してください");
+        } else {
+            false;
+        }
+    })
+)
+form.addEventListener('invalid', function (e) {
+    for (i = 0; i < gender.length; i++) {
+        
+    }
+}, false);*/
+
+
+
--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -8,7 +8,7 @@
     </head>
 
     <body>
-    <!-- <script src="{{ asset('/js/input.js') }}"></script> onSubmit="return checkForm()"　autofocus required title="" -->
+     <!-- onSubmit="return checkForm()"> -->
         @if ($errors->any())
         <div class="alert alert-danger">
 	        <ul>
@@ -20,26 +20,29 @@
         @endif
         <form action="{{ route('post_confirm_screen') }}" name="employee_text" method="POST" >
         <div class = "input">
-           <p><label>社員ID<span>*</span><input type="text" id="employee_id" name='employee_id' Pattern="^YZ[0-9]{8}" placeholder="例）YZ12345678"  value="{{ old('employee_id') }}"></label></P>
+           <p><label>社員ID<span>*</span><input type="text" id="employee_id" name='employee_id' Pattern="^YZ[0-9]{8}" placeholder="例）YZ12345678" minlength= "10" maxlength= "10" autofocus required title="" value="{{ old('employee_id') }}"></label></P>
            
             <div class="name">
-              <p><label>社員名<span>*</span><input type="text" name='first_name' placeholder="姓" style="width: 120px;" maxlength="25" value="{{ old('first_name') }}">
-              <input type="text" name='last_name' placeholder="名" style="width: 120px;" value="{{ old('last_name') }}"></label></p>
+              <p><label>社員名<span>*</span><input type="text" name='first_name' id="first_name" placeholder="姓" maxlength="25" style="width: 120px;" autofocus required title="" value="{{ old('first_name') }}">
+              <input type="text" name='last_name' id="last_name" placeholder="名"  maxlength="25" style="width: 120px;" autofocus required title="" value="{{ old('last_name') }}"></label></p>
              </div>
              <div>
                 <p>所属セクション<span>*</span>
-                <select name = 'department_list' required title="">
-                <option value="">5つの選択肢を表示</option>
-                <option>シス開</option>
-                <option>グロカル</option>
-                <option>ビジソル</option>
+                <select name = 'department_list' id="department_list" autofocus required title="">
+                <option value="">選択してください</option>
+                <option value="シス開">シス開</option>
+                <option value="グロカル">グロカル</option>
+                <option value="ビジソル">ビジソル</option>
                 </select>
             </p>
-           <p><label>メールアドレス<span>*</span><input type="email" name='mail' placeholder="例）taro_yaz@yaz.co.jp"></label></p>
-           <p>性別<span>*</span>
-           <input type="radio" name='gender' value="男">男
-            <input type="radio" name='gender' value="女">女
-            </P>
+           <p><label>メールアドレス<span>*</span><input type="email" name='mail' id="mail" maxlength= "256" Pattern="^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+(\.[a-zA-Z0-9._-]+)+$" placeholder="例）taro_yaz@yaz.co.jp" autofocus required title=""></label></p>
+           
+           <label>性別<span>*</span></label>
+           <div id='radio' >
+           <label for="r_male">男<input type="radio" id ="r_male" name='gender' value="男" class="genders" required></label>
+           <label for="r_female">女<input type="radio" id="r_female" name='gender' class="genders" value="女" required></label>
+           </div>
+
            <p class=notnull><span>*</span>必要項目</p>
            <input type="submit" value="登録する" id="submit">
            <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -48,6 +51,6 @@
 
             <p><a href = "{{ route('menu') }}">メニュー画面に戻る</p>
         </div>
-        <!-- <script src="{{ asset('/js/input.js') }}" defer></script> -->
+        <script src="{{ asset('/js/input.js') }}" defer></script> 
     </body>
 </html>
